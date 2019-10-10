63db9929460f85183a2c4b4927206863f1223a37
Shareef U Din Bhat <shareef.b@tadigital.com>
2019-10-10 15:51:35 +0530

    Added Middleware Thunk for handling async operations

---
+++ src/redux/actions/authorActions.js
@@ -0,0 +1,22 @@
+import * as types from "./actionTypes";
+import * as courseApi from "../../api/authorApi";
+
+export function loadAuthorsSuccess(authors) {
+  return {
+    type: types.LOAD_AUTHORS_SUCCESS,
+    authors: authors
+  };
+}
+
+export function loadAuthors() {
+  return function(dispatch) {
+    return courseApi
+      .getAuthors()
+      .then(authors => {
+        dispatch(loadAuthorsSuccess(authors));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
